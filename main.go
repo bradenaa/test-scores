d7bbf69c53cd4fa050f1d834c213ee5f0ae0d545
bradenaa <bradenaltstatt@gmail.com>
2019-05-05 09:36:44 -0700

    imported an sse client library to handle an incoming event stream. Sent that stream to another channel

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
 
-func main() {
-	http.HandleFunc("/", hello);
-	http.ListenAndServe(":8080", nil);
+	"github.com/r3labs/sse"
+)
+
+func handleEvents(events chan *sse.Event) {
+	// 	// Got some data!
+	for {
+		msg := <-events
+		fmt.Printf("%v", string(msg.Data))
+	}
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello!"))
+}
+
+func main() {
+	// handling event stream in a new channel
+	events := make(chan *sse.Event)
+	client := sse.NewClient("http://live-test-scores.herokuapp.com/scores")
+	client.SubscribeChan("messages", events)
+	go handleEvents(events)
+
+	// establishing a server
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
 }
